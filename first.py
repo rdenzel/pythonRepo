77b986142b475ef88ac7df1a73de927597731e44
= <=>
2013-04-13 14:37:08 -0700

    the first file

---
+++ first.py
@@ -0,0 +1,45 @@
+
+from math import *
+'''
+print ("Hello World!")
+print("Results 2+3 = "+str(2+3))
+#This is a comment
+print("This\nis\nmy\nsample.") # This is also a comment
+print('and now done!')
+'''
+
+'''
+x = input("Select a vlue for x that is between 0 and 100")
+
+#x = sin(0)+cos(0)
+
+if x>100 or x<0:
+	print("x is not in bounds")
+else:
+	print("x = "+str(x))
+
+a= True
+b = False
+
+c = (a==b)
+
+if not c:
+	print(c)
+elif c:
+	print('c is'+str(c))	
+'''
+print "Hello", 100+100
+userName = raw_input("What is your name?")
+userName = str(userName)
+print(userName)
+if userName=="Reece":
+	print("welome back, sir")
+else:
+	print("You do not have access")	
+
+#lisit of operators +,-,*,/,//(floor division, rounds to nearest int),
+#**(to the power of),%(mod)
+
+
+
+print "End Program"
